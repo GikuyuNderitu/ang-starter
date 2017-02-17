3ef97131de13973d2f214e0f7b4c9b67c32b56e7
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-02-16 20:02:07 -0500

    Refactor file structure and naming

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 module.exports = function(app) {
 	'use strict';
-	app.use(express.static(path.join(__dirname,'/../../client/static')))
+	app.use(express.static(path.join(__dirname,'/../../client')))
 	app.use(express.static(path.join(__dirname,'/../../bower_components')))
+	app.use(bodyParser.json())
 	console.log('Middleware initialized');
 };
